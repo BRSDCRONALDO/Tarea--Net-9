965befeac21c9eac8545cdfed480c06f8f03e7b2
BRSDCRONALDO <bsolisd@miumg.edu.gt>
2025-09-16 22:26:10 -0600

    Mensaje descriptivo de los cambios

---
+++ ApiEmpresa/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using ApiEmpresa.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+string? cadena = builder.Configuration.GetConnectionString("ConnectionMySql")??"otracadena"; //ESTA
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<Conexiones>(opt =>
+    opt.UseMySQL(cadena));//ESTA
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+      app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
